8547c7da187f81a2b9a5e4bc2839f1ec2160cc75
Teo Espero <38012835+teoespero@users.noreply.github.com>
2018-11-16 10:49:54 -0800

    Update HRPaySetupDeduct.sql
    PaySetupDeduction – Provide new export with employee and SSN, Vendor Name, Address

--- HRPaySetupDeduct.sql
+++ HRPaySetupDeduct.sql
@@ -2,7 +2,6 @@
 	
 	HRPaySetupDeduct
 	The HRPaySetupDeduct defines the deductions that an employee has, including vendor, optional warrant description, amount or percent and a goal.
-
 */
 
 select 
@@ -14,11 +13,26 @@ from tblDistrict
 select 
 	(select DistrictId from tblDistrict) as OrgId,
 	te.EmployeeID as EmpId,
+	te.fullname,
+	te.socsecno,
 	CONVERT(VARCHAR(10), eff.StartDate, 110) as DateFrom,
 	null as DateThru,
 	null as DeductId,
 	dplan.VendorId as VendorId,
-	ven.DefaultPurchasingAddressId as VendorAddrId,
+	ven.CompanyName,
+	ven.PTTOO,
+	ven.DefaultPurchasingAddressId as DefaultPurchasingAddressId,
+	ad.[Address1],
+	ad.[Address2],
+	ad.City,
+	ad.[State],
+	ad.Zip,
+	ven.DefaultPurchasingAddressId as DefaultPurchasingAddressId,
+	adRemit.[Address1],
+	adRemit.[Address2],
+	adRemit.City,
+	adRemit.[State],
+	adRemit.Zip,
 	pol.[policy] as Descr,
 	pdt.amount as DeductAmt,
 	null as DeductAmtGoal,
@@ -48,6 +62,14 @@ inner join
 	Vendor ven
 	on dplan.VendorId = ven.Id
 inner join
+	[Address] ad
+	on ven.DefaultPurchasingAddressId = ad.Id
+inner join
+	[Address] adRemit
+	on ven.DefaultRemittanceAddressId = adRemit.Id
+inner join
 	DS_Global..PyWithholdingType wht
 	on wht.Id = dplan.PyDeductionTypeId
-
+where
+	te.TerminateDate is null
+	and te.EmployeeID > 0
